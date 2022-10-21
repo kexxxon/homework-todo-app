775a6fc6f1c4be5935b9e1ab29a894a8274f9823
kexxxon <dev.kexon@gmail.com>
2022-10-21 21:55:52 +0900

    Feat: RecommendationsController 구현

---
+++ src/main/java/homework/todo/RecommendationsController.java
@@ -0,0 +1,13 @@
+package homework.todo;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class RecommendationsController {
+    @GetMapping("/")
+    public String helloWorld() {
+
+        return "To-do Application !";
+    }
+}
